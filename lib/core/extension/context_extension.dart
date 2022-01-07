312ffdc804e0a1c963d845132d49d4d57a27d0f3
Bilal Özcan <bilalozcan015@gmail.com>
2022-01-07 23:17:09 +0300

    base app configuration

---
+++ lib/core/extension/context_extension.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+extension ContextExtension on BuildContext {
+  ThemeData get themeData => Theme.of(this);
+
+  double dynamicHeight(double value) => MediaQuery.of(this).size.height * value;
+
+  double dhExceptStatusBar(double value) =>
+      (dynamicHeight(1) - dynamicStatusBarHeight()) * value;
+
+  double dhExceptStatusBarAppBar(double value) =>
+      (dynamicHeight(1) - dynamicStatusBarHeight() - dynamicAppBarHeight(1)) *
+      value;
+
+  double dynamicWidth(double value) => MediaQuery.of(this).size.width * value;
+
+  double dynamicStatusBarHeight({double value = 1}) =>
+      MediaQuery.of(this).padding.top * value;
+
+  double dynamicAppBarHeight(double value) =>
+      AppBar().preferredSize.height * value;
+
+  double dynamicAppBarWidth(double value) =>
+      AppBar().preferredSize.width * value;
+
+  Future navigateTo(Widget widget) async =>
+      Navigator.push(this, MaterialPageRoute(builder: (context) => widget));
+
+  // String formatDateToTime(DateTime? dateTime) =>
+  //     formatDate(dateTime!, [HH, ':', nn],
+  //         locale: TurkishDateLocale());
+  // String customFormatDate2(DateTime? dateTime) =>
+  //     formatDate(dateTime!, [dd, ' ', MM, ' ', yyyy],
+  //         locale: TurkishDateLocale());
+
+  void pop<T extends Object?>([T? result]) => Navigator.pop(this, result);
+}
