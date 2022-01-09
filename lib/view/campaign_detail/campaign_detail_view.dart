9a8b7c54ed0276aad264ad107e12345b21548576
Bilal Özcan <bilalozcan015@gmail.com>
2022-01-09 08:03:57 +0300

    app icon changed

--- lib/view/campaign_detail/campaign_detail_view.dart
+++ lib/view/campaign_detail/campaign_detail_view.dart
@@ -27,125 +27,127 @@ class CampaignDetailView extends StatelessWidget {
                   : Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Container(
-                          height: context.dhExceptStatusBar(1),
-                          child: Stack(
-                            children: [
-                              ListView(
-                                children: [
-                                  Image.network(
-                                      viewModel.campaign!.detailImageUrl!),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 12, vertical: 10),
-                                    child: CustomText(
-                                      viewModel.campaign!.name!,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 24,
+                        Flexible(
+                          child: Container(
+                            height: context.dhExceptStatusBar(1),
+                            child: Stack(
+                              children: [
+                                ListView(
+                                  children: [
+                                    Image.network(
+                                        viewModel.campaign!.detailImageUrl!),
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 12, vertical: 10),
+                                      child: CustomText(
+                                        viewModel.campaign!.name!,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 24,
+                                      ),
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 12, vertical: 10),
-                                    child: Row(
-                                      children: [
-                                        Icon(Icons.calendar_today,
-                                            color: Color(0xff2751B8)),
-                                        SizedBox(
-                                            width: context.dynamicWidth(0.01)),
-                                        CustomText(
-                                          '${context.customFormatDate2(viewModel.campaign!.campaignStartDate)} - ${context.customFormatDate2(viewModel.campaign!.campaignEndDate)}',
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w400,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Container(
-                                    padding:
-                                        EdgeInsets.symmetric(horizontal: 12),
-                                    height:
-                                        context.dhExceptStatusBarAppBar(0.065),
-                                    width: context.dynamicWidth(1),
-                                    decoration: BoxDecoration(
-                                      border: Border(
-                                          top: BorderSide(
-                                              width: 0.3, color: Colors.grey),
-                                          bottom: BorderSide(
-                                              width: 0.3, color: Colors.grey)),
-                                    ),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Row(children: [
-                                          Icon(Icons.location_on,
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 12, vertical: 10),
+                                      child: Row(
+                                        children: [
+                                          Icon(Icons.calendar_today,
                                               color: Color(0xff2751B8)),
+                                          SizedBox(
+                                              width: context.dynamicWidth(0.01)),
                                           CustomText(
-                                              'Kampanya Noktalarını Görüntüle',
-                                              fontSize: 14)
-                                        ]),
-                                        Icon(
-                                          Icons.arrow_forward_ios,
-                                          color: Colors.grey,
-                                          size: 20,
+                                            '${context.customFormatDate2(viewModel.campaign!.campaignStartDate)} - ${context.customFormatDate2(viewModel.campaign!.campaignEndDate)}',
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 12),
+                                      height:
+                                          context.dhExceptStatusBarAppBar(0.065),
+                                      width: context.dynamicWidth(1),
+                                      decoration: BoxDecoration(
+                                        border: Border(
+                                            top: BorderSide(
+                                                width: 0.3, color: Colors.grey),
+                                            bottom: BorderSide(
+                                                width: 0.3, color: Colors.grey)),
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Row(children: [
+                                            Icon(Icons.location_on,
+                                                color: Color(0xff2751B8)),
+                                            CustomText(
+                                                'Kampanya Noktalarını Görüntüle',
+                                                fontSize: 14)
+                                          ]),
+                                          Icon(
+                                            Icons.arrow_forward_ios,
+                                            color: Colors.grey,
+                                            size: 20,
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Html(data: viewModel.campaign!.descriptionHeader!,),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Html(data: viewModel.campaign!.descriptionBody!),
+                                    )
+                                  ],
+                                ),
+                                Positioned(
+                                  top: context.dhExceptStatusBar(0.02),
+                                  right: context.dhExceptStatusBar(0.02),
+                                  child: CircleAvatar(
+                                    child: IconButton(
+                                      icon: Icon(Icons.close),
+                                      onPressed: () => context.pop(),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  child: Container(
+                                    width: context.dynamicWidth(1),
+                                    child: Stack(
+                                      children: [
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Container(
+                                                height: context.dhExceptStatusBar(0.04),
+                                                width: context.dynamicWidth(1),
+                                                color: Colors.red.withOpacity(0)),
+                                            Container(
+                                                height: context.dhExceptStatusBar(0.06),
+                                                width: context.dynamicWidth(1),
+                                                color: Colors.white),
+                                          ],
+                                        ),
+                                        Center(
+                                          child: CustomButton(
+                                            width: context.dynamicWidth(0.9),
+                                            onPressed: () {},
+                                            text: 'Şifreyi Okut, Hemen Katıl',
+                                            textColor: Colors.white,
+                                            backgroundColor: Color(0xff2751B8),
+                                          ),
                                         )
                                       ],
                                     ),
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Html(data: viewModel.campaign!.descriptionHeader!,),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Html(data: viewModel.campaign!.descriptionBody!),
-                                  )
-                                ],
-                              ),
-                              Positioned(
-                                top: context.dhExceptStatusBar(0.02),
-                                right: context.dhExceptStatusBar(0.02),
-                                child: CircleAvatar(
-                                  child: IconButton(
-                                    icon: Icon(Icons.close),
-                                    onPressed: () => context.pop(),
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                bottom: 0,
-                                child: Container(
-                                  width: context.dynamicWidth(1),
-                                  child: Stack(
-                                    children: [
-                                      Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Container(
-                                              height: context.dhExceptStatusBar(0.04),
-                                              width: context.dynamicWidth(1),
-                                              color: Colors.red.withOpacity(0)),
-                                          Container(
-                                              height: context.dhExceptStatusBar(0.06),
-                                              width: context.dynamicWidth(1),
-                                              color: Colors.white),
-                                        ],
-                                      ),
-                                      Center(
-                                        child: CustomButton(
-                                          width: context.dynamicWidth(0.9),
-                                          onPressed: () {},
-                                          text: 'Şifreyi Okut, Hemen Katıl',
-                                          textColor: Colors.white,
-                                          backgroundColor: Color(0xff2751B8),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              )
-                            ],
+                                )
+                              ],
+                            ),
                           ),
                         ),
 
